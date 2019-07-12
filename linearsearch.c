a0c83e14ffac6627477011364fccb5f848fd41e8
kanishka012 <kanishkabansal99@gmail.com>
2019-07-12 09:26:11 -0700

    Helo Git

---
+++ linearsearch.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int arr[100],no,i,n;
+for(i=0;i<n;i++)
+{
+	scanf("%d",&arr[i]);
+}
+for(i=0;i<n;i++)
+{
+	if(arr[i]==no)
+	{
+		printf("%d",no);
+	}
+}
+return 0;
+}
+
