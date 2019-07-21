7f0c0eccb27a1735de36722ad13b507bbecf9146
kanishka012 <kanishkabansal99@gmail.com>
2019-07-21 07:32:06 -0700

    for n number of rotations

--- rightrotation.c
+++ rightrotation.c
@@ -3,21 +3,27 @@
 int main(int argc, const char *argv[])
 {
 	int arr[argc-1],i,k=0;
-	for(i=1;i<argc;i++)
+	for(i=1;i<argc-1;i++)
 	{
 		arr[k]=atoi(argv[i]);
 		k++;
 	}
-	int temp=arr[argc-2];
-	for(i=argc-2;i>0;i--)
-	{
-		arr[i]=arr[i-1];
-	}
-	arr[0]=temp;
-	for(i=0;i<argc-1;i++)
+	int temp;
+	int n=atoi(argv[argc-1]);
+	while(n!=0){
+		temp=arr[argc-3];
+		for(i=argc-3;i>0;i--)
+		{
+			arr[i]=arr[i-1];
+		}
+		arr[0]=temp;
+		n--;
+		}
+	for(i=0;i<argc-2;i++)
 	{
 		printf("%d",arr[i]);
 	}
+	
 return 0;
 }
 	
