83fe02096572a0a5275670a2c63bdd322fc8d7e4
kanishka012 <kanishkabansal99@gmail.com>
2019-07-21 10:44:07 -0700

    sorted array is printed

---
+++ sorted_array.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc,const char *argv[])
+{
+        int arr[argc-1],i,k=0;
+        for(i=1;i<argc;i++)
+        {
+                arr[k]=atoi(argv[i]);
+                k++;
+        }
+        while(1)
+        {
+                int flag=0;
+                for(i=0;i<argc-2;i++)
+                {
+                        if(arr[i]>arr[i+1])
+                        {
+                                flag=1;
+                                break;
+                        }
+                }
+                if(flag==1)
+                {
+                        int temp=arr[0];
+                        for(i=0;i<argc-2;i++)
+                        {
+                                arr[i]=arr[i+1];
+                        }
+                        arr[argc-2]=temp;
+                }
+                if(flag==0)
+                break;
+        }
+	for(i=0;i<argc-1;i++)
+	{
+        printf("%d",arr[i]);
+	}
+return 0;
+}
+
