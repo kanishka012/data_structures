565312ef30a5f6ea75ef1974cfa422fe811fe94d
kanishka012 <kanishkabansal99@gmail.com>
2019-08-08 19:38:00 -0700

    minor diagonal

---
+++ minor_diagonal.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+int main(int argc,const char *argv[])
+{
+        int n=atoi(argv[1]);
+        int i,j,k=2,arr[n][n];
+        for(i=0;i<n;i++)
+        {
+                for(j=0;j<n;j++)
+                {
+                        arr[i][j]=atoi(argv[k]);
+                        k++;
+                }
+        }
+                for(i=0;i<n;i++)
+                {
+                        for(j=0;j<n;j++)
+                        {
+                                if(i+j==n-1)
+                                {
+                                        cout<<arr[i][j]<<" ";
+                                }
+                        }
+                }
+}
+     
